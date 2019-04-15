eb4c5423d1ac1f95471c76e18ebee6f5d0a1f15d
Erica L <46460064+ericaleon@users.noreply.github.com>
2019-04-15 00:34:33 -0400

    update to flask app

--- bee_flask.py
+++ bee_flask.py
@@ -42,42 +42,51 @@ def welcome():
     return (
         f"Welcome, honeybees! <br/>"
         f"Available Routes:<br/>"
-        f"/api/honey-yield<br/>"
-        f"/api/greatest-change"
+        f"/api/2015/honey-yield<br/>"
+        f"/api/2015/pesticides-colonies"
     )
 
 
-@app.route("/api/honey-yield")
+@app.route("/api/2015/honey-yield")
 def honey():
     """Returns a list of states' honey yield per colony"""
     # Query all states' bee colony & honey production data in descending order
-    sel = "bees.state, bees.end_colonies,bees.colony_yield, bees.production"
+    sel = [bees.state, bees.end_colonies,bees.colony_yield, bees.production]
     results = session.query(*sel).\
         order_by(bees.colony_yield.desc()).\
         all()
 
-    # Convert list of tuples into normal list
-    all_prod = list(np.ravel(results))
+    # create dictionary of row data and append to list for production in all states
+    all_prod = []
+    for state, end_colonies, colony_yield, production in results:
+        prod_dict = {}
+        prod_dict["state (abbr.)"] = state
+        prod_dict["colony count (end of year)"] = end_colonies
+        prod_dict["yield per colony"] = colony_yield
+        prod_dict["production"] = production
+        all_prod.append(prod_dict)
 
     return jsonify(all_prod)
 
 
-@app.route("/api/greatest-change")
+@app.route("/api/2015/pesticides-colonies")
 def change():
     """Return a list of colony change data including the state, colony change, and pesticide use"""
     # Query bees and pesticides tables
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    sel = [bees.state, bees.colony_change, pests.compound, pests.high_estimate]
+    results = session.query(*sel).filter(bees.state == pests.state).all()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    # Create a dictionary from the row data and append to a list of colony change and pesticide use
+    change_pest = []
+    for state, colony_change, compound, high_estimate in results:
+        chg_p_dict = {}
+        chg_p_dict["state"] = state
+        chg_p_dict["colony change"] = colony_change
+        chg_p_dict["compound"] = compound
+        chg_p_dict["high estimate"] = high_estimate
+        change_pest.append(chg_p_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(change_pest)
 
 
 if __name__ == '__main__':
